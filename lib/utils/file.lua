f89face5d6e36dfb75620372e3e4d2c6c2767616
Liew Xun <damakuno@gmail.com>
2024-05-24 00:41:06 +0800

    add json support

---
+++ lib/utils/file.lua
@@ -0,0 +1,19 @@
+local file
+file = {
+    -- Read an entire file.
+    -- Use "a" in Lua 5.3; "*a" in Lua 5.1 and 5.2
+    readall = function (filename)
+        local fh = assert(io.open(filename, "rb"))
+        local contents = assert(fh:read(_VERSION <= "Lua 5.2" and "*a" or "a"))
+        fh:close()
+        return contents
+    end,
+    -- Write a string to a file.
+    write = function (filename, contents)
+        local fh = assert(io.open(filename, "wb"))
+        fh:write(contents)
+        fh:flush()
+        fh:close()
+    end
+}  
+return file
--- main.lua
+++ main.lua
@@ -1,12 +1,17 @@
 LIP = require "lib.utils.LIP"
 ComicPanel = require "lib.utils.ComicPanel"
+json = require "lib.utils.json"
+file = require "lib.utils.file"
 
 mouse = { x = 0, y = 0, dx = 0, dy = 0, pressed = false }
 
 function love.load()
-    love.window.setTitle("Blood Thread")    
+    love.window.setTitle("Blood Thread")
     love.window.setMode(1920, 1080)
     settings = LIP.load("config/Settings.ini")
+    -- debug_text = file.readall('config/test.json')
+    test = json.decode(file.readall('config/test.json'))
+    debug_text = test.Misc.debug
     panel1 = ComicPanel:new(love.graphics.newImage("res/test/panel1.png"))
 end
 
@@ -19,8 +24,8 @@ function love.draw()
         end
         love.graphics.print("x: "..mouse.x.." y: "..mouse.y, 500, 40)
     end
-    panel1:draw(40, 40)
-    -- love.graphics.setColor(255 / 255, 255 / 255, 255 / 255, 1)
+    -- panel1:draw(40, 40)
+    -- love.graphics.setColor(255 / 255, 255 / 255, 255 / 255, 1)    
 end
 
 function love.update(dt)
@@ -33,3 +38,10 @@ function love.mousemoved(x, y, dx, dy, istouch)
     mouse.dx = dx
     mouse.dy = dy    
 end
+
+function love.keypressed(key, u)
+    --Debug
+    if key == "rctrl" then --set to whatever key you want to use
+       debug.debug()
+    end
+ end
